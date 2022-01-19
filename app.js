91245331d18d02265fff8ffcadd92c82af71ea2b
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-01-19 17:16:37 +0330

    First commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const config = require("./config.json");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("views", __dirname + "/templates");
+app.use(express.static(__dirname + "/public")); //__dir and not _dir
+app.set("view engine", "ejs");
+
+MongoClient.connect(config.base.db_url, async (err, client) => {
+  if (err) throw err;
+
+  const db = client.db("Mpm-main");
+  console.log(`Connected to DataBase`);
+  const pageInformation = db.collection("pageInformation").find({}).toArray();
+  console.log(pageInformation);
+
+  app.get("/", (req, res) => {
+    res.render("index", { pageInformation });
+  });
+});
+
+app.listen(config.base.port, () => {
+  console.log(`Server is running on ${config.base.port}`);
+});
