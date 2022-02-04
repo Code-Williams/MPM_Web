a7d775782cb3771014a330d92aea257cb2b4fa09
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-02-04 16:31:51 +0330

    Routes changed to MVC

--- app.js
+++ app.js
@@ -1,45 +1,17 @@
 const express = require("express");
-const mysql = require("mysql");
 const config = require("./config.json");
 const bodyParser = require("body-parser");
 
 const app = express();
 
-const dbOptions = {
-  host: config.database.host,
-  user: config.database.user,
-  password: config.database.password,
-}
-
-if(config.database.name && config.database.name !== ""){
-    dbOptions['name'] = config.database.name
-}
-
-const conn = mysql.createConnection(dbOptions);
-
-conn.connect((err, res) => {
-  if (err) throw err;
-  console.log("Connected to DataBase");
-});
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set("views", __dirname + "/templates");
+app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + "/public")); //__dir and not _dir
 app.set("view engine", "ejs");
 
-const registerRouter = require("./routes/registerRouter");
-app.use("/register", registerRouter);
-
-const loginRouter = require("./routes/loginRouter");
-app.use("/login", loginRouter);
-
-const dashboardRouter = require("./routes/dashboardRouter");
-app.use("/dashboard", dashboardRouter);
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
+const routes = require("./routes")
+app.use("/", routes)
 
 app.listen(config.app.port, () => {
   console.log(`Server is running on ${config.app.port}`);
---
+++ routes/index.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const Router = express.Router()
+
+Router.get("/", (req, res) => {
+    res.render("index")
+})
+
+Router.get("/about", (req, res) => {
+    res.render("about")
+})
+
+Router.get("/cart", (req, res) => [
+    res.render("cart")
+])
+
+Router.get("/checkout", (req, res) => {
+    res.render("checkout")
+})
+
+Router.get("/contact", (req, res) => {
+    res.render("contact")
+})
+
+Router.get("/login", (req, res) => [
+    res.render("login")
+])
+
+Router.get("/register", (req, res) => {
+    res.render("register")
+})
+
+Router.get("/news", (req, res) => {
+    res.render("news")
+})
+
+Router.get("/single-news", (req, res) => {
+    res.render("single-news")
+})
+
+Router.get("/single-product", (req, res) => {
+    res.render("single-product")
+})
+
+Router.get("/dashboard", (req, res) => {
+    res.render("user_interface")
+})
+
+module.exports = Router
