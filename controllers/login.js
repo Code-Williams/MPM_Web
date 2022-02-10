e26705ff8ed3b56b9477056ab5e55cdddc246682
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-02-10 13:19:56 +0330

    Login and register completed

--- controllers/login.js
+++ controllers/login.js
@@ -1,9 +1,20 @@
 const aboutMain = require("../models/About")
+const passport = require("passport")
 
-const login = async (req, res) => {
+const get = async (req, res) => {
     const about = await aboutMain.findAll()
-
-    res.render("login", {about})
+    
+    res.render("login", {about, flash : req.flash().error})
 }
 
-module.exports = login;
+const post = passport.authenticate("local", {
+    successRedirect : "/",
+    failureRedirect : "/login",
+    failureFlash : true,
+    session : false
+})
+
+module.exports = {
+    get,
+    post
+};
--- controllers/register.js
+++ controllers/register.js
@@ -1,9 +1,31 @@
 const aboutMain = require("../models/About")
+const User = require("../models/User")
 
-const register = async (req, res) => {
+const get = async (req, res) => {
     const about = await aboutMain.findAll();
 
     res.render("register", {about})
 }
 
-module.exports = register
+const post = async (req, res) => {
+    const about = await aboutMain.findAll();
+
+    if(req.body.password !== req.body['password-retype']) return res.render("register", {error : "کلمه عبور با تکرار آن یکسان نیست.", about});
+
+    const newUser = await User.create({
+        username : req.body.username,
+        password : req.body.password,
+        email : req.body.email,
+        firstName : req.body.firstName,
+        lastName : req.body.lastName,
+        number : req.body.number
+    })
+
+    res.redirect("/login")
+
+}
+
+module.exports = {
+    get, 
+    post
+}
