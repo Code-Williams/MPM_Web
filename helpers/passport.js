9303b870a29634053d91d53fec5ff80add5c68d3
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-02-10 13:19:13 +0330

    Passport added

---
+++ helpers/passport.js
@@ -0,0 +1,33 @@
+const passport = require("passport")
+const localStrategy = require("passport-local").Strategy
+const User = require("../models/User")
+
+passport.initialize()
+
+passport.use(new localStrategy(
+  async function (username, password, done){
+
+    const user = await User.findOne({
+      where : {
+        username : username
+      }
+    })
+
+    try {
+
+      if(!user){
+        return done(null, false, { message : "نام کاربری و یا کلمه عبور اشتباه است" })
+      }
+
+      if(!User.validPassword(user, password)){
+        return done(null, false, { message : "نام کاربری و یا کلمه عبور اشتباه است" })
+      }
+
+      return done(null, user)
+      
+    } catch (error) {
+      done(error)
+    }
+
+  }
+))
