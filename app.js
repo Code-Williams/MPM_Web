c5d35cde226bb92aba8df94887ee2dcb369957db
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-02-10 13:19:29 +0330

    Clean code and user system added

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const config = require("./config.json");
 const bodyParser = require("body-parser");
+const flash = require("connect-flash")
+const session = require("express-session")
+const cookieParser = require("cookie-parser")
+
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(flash())
+app.use(cookieParser())
+app.use(session({ secret: config.secret }))
 app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + "/public")); //__dir and not _dir
 app.set("view engine", "ejs");
 
-const routes = require("./routes")
+require("./helpers/passport")
+
+const routes = require("./routes");
+const user = require("./models/User");
 app.use("/", routes)
 
 app.listen(config.app.port, () => {
