35ae2b4ff7ca5efc14768219e5d716e67db0f0a4
Shayan Nasrabadi <williams.ca001@gmail.com>
2022-02-14 00:42:06 +0330

    orders page base completed

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,6 +1,8 @@
 const About = require("../models/About")
 const User = require("../models/User")
 const Address = require("../models/Address")
+const Order = require("../models/Order")
+const Product = require("../models/Products")
 
 const dashboardController = async (req, res) => {
     const about = await About.findAll();
@@ -23,11 +25,15 @@ const orders = async (req, res) => {
     const about = await About.findAll();
     const user = await User.findByPk(req.user.id);
     const tab = req.query.activeTab || "in_progress"
+    const results = await Order.findAll({ where : {userId : req.user.id, status : tab} })
+    const products = await Product.findAll();
 
     res.render("dashboardOrder", {
         about,
         user,
         tab,
+        results,
+        products,
         flash : req.flash()
     })
 }
